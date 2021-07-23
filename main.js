752e6ba64f2bf4574039d3ebcd7f6f0fe9d697ca
Geokola <kolalife4geo@gmail.com>
2021-07-23 09:59:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@
 var btn = document.querySelector("#btn");
 var show = document.querySelector("#show");
 btn.addEventListener('click', function(){
-	show.innerHTML = "Today is my birthday!" + "<br>" + "Celebrate with me";
+	show.innerHTML = "Today is my birthday!!!";
 });
