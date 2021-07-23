89b695031f3216b7165f2e2551c2f92a5961ea4e
Geokola <kolalife4geo@gmail.com>
2021-07-23 09:42:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+var btn = document.querySelector("#btn");
+var show = document.querySelector("#show");
+var now = new Date();
+btn.addEventListener('click', function(){
+	show.innerHTML = "Today";
+})
