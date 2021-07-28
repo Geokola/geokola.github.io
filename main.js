ceec45d0ac302e3b92ad4204bfa6d1d0f74bb697
Geokola <kolalife4geo@gmail.com>
2021-07-28 01:53:19 +0100

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-
-var btn = document.querySelector("#btn");
-var show = document.querySelector("#show");
-btn.addEventListener('click', function(){
-	show.innerHTML = "Today is my birthday!!!";
-});
